3cf960915b6cff0744c84db4761e95889a1718c2
RickIX <118491601+RickIX@users.noreply.github.com>
2023-04-02 21:51:23 -0300

    Add files via upload

---
+++ Pacotes/src/br/edu/up/persistencia/ContaPersistencia.java
@@ -0,0 +1,32 @@
+package br.edu.up.persistencia;
+import br.edu.up.entidades.Conta;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContaPersistencia {
+	private static List<Conta> contas = new ArrayList<Conta>();
+	
+	public static void incluirConta(Conta objConta) {
+		contas.add(objConta);
+	}
+	
+	public static Conta buscarConta(Conta conta) {
+		for(Conta objConta: contas) {
+			if(objConta.getNumeroAgencia() == conta.getNumeroAgencia() && objConta.getNumeroConta() == conta.getNumeroConta()) {
+				return objConta;
+			}
+		}
+		return null;
+	}
+	
+	public static void atualizarConta(Conta conta) {
+		int i = 0;
+		for(Conta objConta: contas) {
+			if(objConta.getNumeroAgencia() == conta.getNumeroAgencia() && objConta.getNumeroConta() == conta.getNumeroConta()) {
+				contas.set(i, conta);
+				return;
+			}
+			i++;
+		}
+	}
+}
